3e7df9395e955ca7d5703742f8c2b4d98faf76bb
BerCad <bernardo.c1995@aluno.ifsc.edu.org.br>
2023-06-25 22:42:28 -0300

    Edições no componente de Dashboard.

---
+++ SQUAD-Front-End-main/src/Components/Dashboard.js
@@ -0,0 +1,65 @@
+//CSS
+import "./Dashboard.css";
+
+//REACT
+import React, { useEffect, useState } from "react";
+
+function formatApiResponse(responseData) {
+  try {
+    const data = JSON.parse(responseData);
+
+    // Formatar os dados conforme necessário
+    let formattedData = "";
+
+    for (let animal in data) {
+      formattedData += animal + ":";
+
+      for (let faixaEtaria in data[animal]) {
+        formattedData += ` ${faixaEtaria}:`;
+
+        for (let item in data[animal][faixaEtaria]) {
+          formattedData += ` ${item} (${data[animal][faixaEtaria][item]})`;
+        }
+      }
+      formattedData += "; ";
+    }
+
+    return formattedData;
+  } catch (error) {
+    console.log("Error parsing API response:", error);
+    return "Error: Invalid API response";
+  }
+}
+
+const Dashboard = () => {
+  const [responseData, setResponseData] = useState("");
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await fetch(
+          "http://localhost:8080/animais/agruparTodos"
+        );
+        const result = await response.text();
+        setResponseData(result);
+      } catch (error) {
+        console.log("error", error);
+      }
+    }
+
+    fetchData();
+  }, []);
+
+  const formattedResponse = formatApiResponse(responseData);
+
+  return (
+    <div className="Dashboard">
+      <div className="lista-do-dashboard">
+        <h1>Dashboard</h1>
+        <p>{formattedResponse}</p>
+      </div>
+    </div>
+  );
+};
+
+export default Dashboard;
