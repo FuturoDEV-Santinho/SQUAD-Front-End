cf8072fe6150dc2ab8ddef19025b28e369c1ab38
BerCad <bernardo.c1995@aluno.ifsc.edu.org.br>
2023-06-25 22:36:21 -0300

    Adicionado o componente para edição do armazem.

---
+++ SQUAD-Front-End-main/src/Components/ArmazemEdicao.js
@@ -0,0 +1,57 @@
+import React, { useState } from "react";
+
+const ArmazemEdicao = ({
+  id,
+  nomeAtual,
+  tipoAnimalAtual,
+  onEditar,
+  onCancelar,
+}) => {
+  const [novoNome, setNovoNome] = useState(nomeAtual);
+  const [novoTipoAnimal, setNovoTipoAnimal] = useState(tipoAnimalAtual);
+
+  const handleNomeChange = (e) => {
+    setNovoNome(e.target.value);
+  };
+
+  const handleTipoAnimalChange = (e) => {
+    setNovoTipoAnimal(e.target.value);
+  };
+
+  const handleEditar = () => {
+    const novoArmazem = {
+      nome: novoNome,
+      tipoAnimal: novoTipoAnimal,
+    };
+
+    onEditar(id, novoArmazem);
+  };
+
+  return (
+    <div>
+      <h2>Editar Armazém</h2>
+      <form>
+        <div>
+          <label>Nome:</label>
+          <input type="text" value={novoNome} onChange={handleNomeChange} />
+        </div>
+        <div>
+          <label>Tipo de Animal:</label>
+          <select value={novoTipoAnimal} onChange={handleTipoAnimalChange}>
+            <option value="">Selecione</option>
+            <option value="GATO">GATO</option>
+            <option value="CACHORRO">CACHORRO</option>
+          </select>
+        </div>
+        <button type="button" onClick={handleEditar}>
+          Editar
+        </button>
+        <button type="button" onClick={onCancelar}>
+          Cancelar
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default ArmazemEdicao;
